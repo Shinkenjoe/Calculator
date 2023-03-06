ee83a8ad57cfc98fef7f98d8173e091acd9e3e33
shinkenjoe <shinkenjoe@posteo.in>
2023-03-06 08:27:52 +0100

    Added subtraction method

--- Calculator.java
+++ Calculator.java
@@ -12,4 +12,12 @@ public class Calculator() {
     public float addition(float a, float b){
         return a + b;
     }
+
+    public int subtraction(int a, int b){
+        return a - b;
+    }
+    
+    public double subtraction(double a, double b){
+        return a - b;
+    }
 }
